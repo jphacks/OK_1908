de7500a0f820aed061bd54ab3a302dec5749ec37
kanekou <e165726@ie.u-ryukyu.ac.jp>
2019-10-26 15:56:22 +0900

    Add morphemes page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'analysis', to: 'morphemes#analysis'
   root 'morphemes#analysis'
-  post '/', to: 'morphemes#post_goo'
 end
