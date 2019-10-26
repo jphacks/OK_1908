d820251154715a93314fe0d4c5cdc3972cc9c4b8
kanekou <e165726@ie.u-ryukyu.ac.jp>
2019-10-26 12:20:25 +0900

    Add analysis controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'analysis', to: 'morphemes#analysis'
+  root 'morphemes#analysis'
 end
